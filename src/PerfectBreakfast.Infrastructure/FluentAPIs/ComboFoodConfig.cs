045a38d522bbc25d17a2786f6b900f1ff23eae04
viethung <viethungdev23@gmail.com>
2024-01-03 16:24:22 +0700

    #21 add Combo,ComboFood entities

---
+++ src/PerfectBreakfast.Infrastructure/FluentAPIs/ComboFoodConfig.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PerfectBreakfast.Domain.Entities;
+
+namespace PerfectBreakfast.Infrastructure.FluentAPIs;
+
+public class ComboFoodConfig : IEntityTypeConfiguration<ComboFood>
+{
+    public void Configure(EntityTypeBuilder<ComboFood> builder)
+    {
+        builder.ToTable("ComboFood");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+        
+        builder.HasOne(x => x.Combo)
+            .WithMany(x => x.ComboFoods)
+            .HasForeignKey(fk => fk.ComboId);
+        
+        builder.HasOne(x => x.Food)
+            .WithMany(x => x.ComboFoods)
+            .HasForeignKey(fk => fk.FoodId);
+    }
+}
