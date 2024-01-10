c84fa4db61be70e54594a00db7c914b70c28fc18
AnhTuanTran <tranvuonganhtuan@gmail.com>
2024-01-10 15:54:16 +0700

    Add id deliveryUnit-ManagerUnits
    Add id deliveryUnit-ManagerUnits

--- src/PerfectBreakfast.Application/Models/DeliveryUnitModels/Response/DeliveryUnitResponseModel.cs
+++ src/PerfectBreakfast.Application/Models/DeliveryUnitModels/Response/DeliveryUnitResponseModel.cs
@@ -2,6 +2,8 @@ namespace PerfectBreakfast.Application.Models.DeliveryUnitModels.Response;
 
 public record DeliveryUnitResponseModel
 {
+    
+    public Guid? Id { get; set; } 
     public string Name { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
 }
