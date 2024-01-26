1c090e2a51aa914d3842603f93f17e204d01d9e5
PhiLong0103 <longnhpse151366@fpt.edu.vn>
2024-01-26 19:22:43 +0700

    Validate combo, menu, food, company

--- src/PerfectBreakfast.API/Validations/Combo/UpdateComboValidator.cs
+++ src/PerfectBreakfast.API/Validations/Combo/UpdateComboValidator.cs
@@ -3,14 +3,15 @@ using PerfectBreakfast.Application.Models.ComboModels.Request;
 
 namespace PerfectBreakfast.API.Validations.Combo
 {
-    public class ComboValidator : AbstractValidator<CreateComboRequest>
+    public class UpdateComboValidator : AbstractValidator<UpdateComboRequest>
     {
-        public ComboValidator()
+        public UpdateComboValidator()
         {
             RuleFor(x => x.Name).NotNull().NotEmpty().WithMessage("Name can not null or empty");
             RuleFor(x => x.Content).NotNull().NotEmpty().WithMessage("Content can not null or empty");
             RuleFor(x => x.Image).NotNull().NotEmpty().WithMessage("Image can not null or empty");
             RuleFor(x => x.FoodId).NotNull().NotEmpty().WithMessage("Food can not null or empty");
         }
+
     }
 }
---
+++ src/PerfectBreakfast.API/Validations/Food/UpdateFoodValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using PerfectBreakfast.Application.Models.FoodModels.Request;
+
+namespace PerfectBreakfast.API.Validations.Food;
+
+public class UpdateFoodValidator : AbstractValidator<UpdateFoodRequestModels>
+{
+    public UpdateFoodValidator()
+    {
+        RuleFor(x => x.Name).NotNull().NotEmpty().WithMessage("Name can not null or empty");
+        RuleFor(x => x.Price).NotNull().NotEmpty().WithMessage("Price can not null or empty");
+        RuleFor(x => x.Image).NotNull().NotEmpty().WithMessage("Image can not null or empty");
+    }
+}
