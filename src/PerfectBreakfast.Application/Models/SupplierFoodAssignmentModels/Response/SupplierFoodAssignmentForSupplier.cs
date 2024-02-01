0d2d16c4c7495021351eb9d8918e10814afecb83
PhiLong0103 <longnhpse151366@fpt.edu.vn>
2024-02-01 18:21:06 +0700

    Add folder

---
+++ src/PerfectBreakfast.Application/Models/SupplierFoodAssignmentModels/Response/SupplierFoodAssignmentForSupplier.cs
@@ -0,0 +1,7 @@
+﻿namespace PerfectBreakfast.Application.Models.SupplierFoodAssignmentModels.Response;
+
+public record SupplierFoodAssignmentForSupplier
+{
+    public string? PartnerName { get; set; }
+    public List<FoodAssignmentResponse> FoodAssignmentResponses { get; set; } = null;
+}
