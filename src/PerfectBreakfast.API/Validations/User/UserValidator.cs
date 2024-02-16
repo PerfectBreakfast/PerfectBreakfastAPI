82cef77e4e5f43de74a1cc2f5e0b587d3444ca19
viethung <viethungdev23@gmail.com>
2024-02-16 09:48:05 +0000

    fix/create-user

--- src/PerfectBreakfast.API/Validations/User/UserValidator.cs
+++ src/PerfectBreakfast.API/Validations/User/UserValidator.cs
@@ -8,11 +8,11 @@ public class UserValidator : AbstractValidator<CreateUserRequestModel>
     public UserValidator()
     {
         RuleFor(p => p.Email).NotEmpty().WithMessage("Email cannot be empty")
-            .Matches(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").WithMessage("Invalid email format");
+            .Matches(@"^[a-z][a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").WithMessage("Invalid email format");
 
         RuleFor(p => p.Name).NotEmpty().WithMessage("fullname cannot be empty")
             .MaximumLength(50)
-            .Matches(@"^[a-zA-Z]+$").WithMessage("Invalid Name format");
+            .Matches(@"^[\p{L}\s]+$").WithMessage("Invalid Name format");
         
         RuleFor(p => p.PhoneNumber).NotEmpty().NotNull().WithMessage("Phone number not null")
             .Matches(@"^0\d{9}$").WithMessage("Invalid phone number format");
