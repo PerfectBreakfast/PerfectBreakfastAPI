04d2ad4463dd981f72233daeffe846250cfdfd1a
AnhTuanTran <tranvuonganhtuan@gmail.com>
2024-02-21 10:54:56 +0700

    UPDATE2
    UPDATE2

--- src/PerfectBreakfast.API/Controllers/V1/CategoryController.cs
+++ src/PerfectBreakfast.API/Controllers/V1/CategoryController.cs
@@ -7,63 +7,66 @@ using PerfectBreakfast.Application.Models.RoleModels.Request;
 using PerfectBreakfast.Application.Services;
 using PerfectBreakfast.Application.Utils;
 
-namespace PerfectBreakfast.API.Controllers.V1
-{
-    [Route("api/v{version:apiVersion}/categories")]
-    public class CategoryController : BaseController
-    {
+namespace PerfectBreakfast.API.Controllers.V1;
 
-        private readonly ICategoryService _categoryService;
-        public CategoryController(ICategoryService categoryService)
-        {
-            _categoryService = categoryService;
-        }
-        /// <summary>
-        /// API FOR SUPPER ADMIN
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet,Authorize(Policy = ConstantRole.RequireSuperAdminRole)]
-        public async Task<IActionResult> GetAllCategory()
-        {
-            var response = await _categoryService.GetAllCategorys();
-            return response.IsError ? HandleErrorResponse(response.Errors) : Ok(response.Payload);
-        }
-        [HttpGet("{id}"),Authorize(Policy = ConstantRole.RequireSuperAdminRole)]
-        public async Task<IActionResult> GetCategoryById(Guid id)
-        {
-            var response = await _categoryService.GetCategoryId(id);
-            return response.IsError ? HandleErrorResponse(response.Errors) : Ok(response.Payload);
-        }
-        
-        /// <summary>
-        /// API FOR SUPPER ADMIN
-        /// </summary>
-        /// <returns></returns>
-        [HttpPost,Authorize(Policy = ConstantRole.RequireSuperAdminRole)]
-        public async Task<IActionResult> CreateCategory(CreateCategoryRequest requestModel)
-        {
-            var response = await _categoryService.CreateCategory(requestModel);
-            return response.IsError ? HandleErrorResponse(response.Errors) : Ok(response.Payload);
-        }
-        /// <summary>
-        /// API FOR SUPPER ADMIN
-        /// </summary>
-        /// <returns></returns>
-        [HttpPut("{id}"),Authorize(Policy = ConstantRole.RequireSuperAdminRole)]
-        public async Task<IActionResult> UpdateCategory(Guid id, UpdateCategoryRequest requestModel)
-        {
-            var response = await _categoryService.UpdateCategory(id, requestModel);
-            return response.IsError ? HandleErrorResponse(response.Errors) : Ok(response.Payload);
-        }
-        /// <summary>
-        /// API FOR SUPPER ADMIN
-        /// </summary>
-        /// <returns></returns>
-        [HttpDelete("{id}"),Authorize(Policy = ConstantRole.RequireSuperAdminRole)]
-        public async Task<IActionResult> RemoveCategory(Guid id)
-        {
-            var response = await _categoryService.RemoveCategory(id);
-            return response.IsError ? HandleErrorResponse(response.Errors) : Ok(response.Payload);
-        }
+[Route("api/v{version:apiVersion}/categories")]
+public class CategoryController : BaseController
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoryController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    /// <summary>
+    /// API FOR SUPPER ADMIN
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet, Authorize(Policy = ConstantRole.RequireSuperAdminRole)]
+    public async Task<IActionResult> GetAllCategory()
+    {
+        var response = await _categoryService.GetAllCategorys();
+        return response.IsError ? HandleErrorResponse(response.Errors) : Ok(response.Payload);
+    }
+
+    [HttpGet("{id}"), Authorize(Policy = ConstantRole.RequireSuperAdminRole)]
+    public async Task<IActionResult> GetCategoryById(Guid id)
+    {
+        var response = await _categoryService.GetCategoryId(id);
+        return response.IsError ? HandleErrorResponse(response.Errors) : Ok(response.Payload);
+    }
+
+    /// <summary>
+    /// API FOR SUPPER ADMIN
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost, Authorize(Policy = ConstantRole.RequireSuperAdminRole)]
+    public async Task<IActionResult> CreateCategory(CreateCategoryRequest requestModel)
+    {
+        var response = await _categoryService.CreateCategory(requestModel);
+        return response.IsError ? HandleErrorResponse(response.Errors) : Ok(response.Payload);
+    }
+
+    /// <summary>
+    /// API FOR SUPPER ADMIN
+    /// </summary>
+    /// <returns></returns>
+    [HttpPut("{id}"), Authorize(Policy = ConstantRole.RequireSuperAdminRole)]
+    public async Task<IActionResult> UpdateCategory(Guid id, UpdateCategoryRequest requestModel)
+    {
+        var response = await _categoryService.UpdateCategory(id, requestModel);
+        return response.IsError ? HandleErrorResponse(response.Errors) : Ok(response.Payload);
+    }
+
+    /// <summary>
+    /// API FOR SUPPER ADMIN
+    /// </summary>
+    /// <returns></returns>
+    [HttpDelete("{id}"), Authorize(Policy = ConstantRole.RequireSuperAdminRole)]
+    public async Task<IActionResult> RemoveCategory(Guid id)
+    {
+        var response = await _categoryService.RemoveCategory(id);
+        return response.IsError ? HandleErrorResponse(response.Errors) : Ok(response.Payload);
     }
 }
