b7c7fc97e66bcb93e4f3f80e886124c6d8a9aa96
PhiLong0103 <longnhpse151366@fpt.edu.vn>
2024-02-28 16:50:22 +0700

    fix update menu

---
+++ src/PerfectBreakfast.Infrastructure/Repositories/MenuFoodRepository.cs
@@ -0,0 +1,12 @@
+﻿using PerfectBreakfast.Application.Interfaces;
+using PerfectBreakfast.Application.Repositories;
+using PerfectBreakfast.Domain.Entities;
+
+namespace PerfectBreakfast.Infrastructure.Repositories;
+
+public class MenuFoodRepository : GenericRepository<MenuFood>, IMenuFoodRepository
+{
+    public MenuFoodRepository(AppDbContext context, ICurrentTime timeService, IClaimsService claimsService) : base(context, timeService, claimsService)
+    {
+    }
+}
