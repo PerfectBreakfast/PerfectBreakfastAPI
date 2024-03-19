ef0a20aecbbef3a4becc74fff02e001b18a3c76f
viethung <viethungdev23@gmail.com>
2024-03-19 15:35:12 +0000

    fix-get-food-by-id

--- src/PerfectBreakfast.Application/Models/FoodModels/Response/FoodResponeCategory.cs
+++ src/PerfectBreakfast.Application/Models/FoodModels/Response/FoodResponeCategory.cs
@@ -8,5 +8,6 @@ public record FoodResponeCategory
     public string Name { get; set; } = string.Empty;
     public decimal Price { get; set; }
     public string Image { get; set; } = string.Empty;
+    public int FoodStatus { get; set; }
     public CategoryResponse? CategoryResponse { get; set; }
 }
