07daf95e146c201a2907911ace91b8973119c3c9
viethung <viethungdev23@gmail.com>
2024-03-20 03:08:29 +0000

    add-index-companyName

--- src/PerfectBreakfast.Infrastructure/Migrations/20240320030549_index-company-nam.Designer.cs
+++ src/PerfectBreakfast.Infrastructure/Migrations/20240320030549_index-company-nam.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PerfectBreakfast.Infrastructure;
 
@@ -10,9 +11,11 @@ using PerfectBreakfast.Infrastructure;
 namespace PerfectBreakfast.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240320030549_index-company-nam")]
+    partial class indexcompanynam
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/PerfectBreakfast.Infrastructure/Migrations/20240320030549_index-company-nam.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PerfectBreakfast.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class indexcompanynam : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Company_Name",
+                table: "Company",
+                column: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Company_Name",
+                table: "Company");
+        }
+    }
+}
